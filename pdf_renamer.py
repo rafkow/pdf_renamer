936d694925c890844f569a10071c92a0160100f5
Rafał Kowalewski <rkowalewski@pl.indorama.net>
2022-03-10 18:34:54 +0100

    add lont extensions

--- pdf_renamer.py
+++ pdf_renamer.py
@@ -3,17 +3,26 @@ import os
 import slate3k
 import shutil
 
-def get_dlv(name):
+
+def get_ext(name):
     with open(name, 'rb') as f:
         doc = slate3k.PDF(f)
         dlv = 'DLV[\d]{5}'
         result = re.search(dlv, doc[0])
-        return result.group(0)
+        if result:
+            return result.group(0)
+        lontl = 'LONTL[\d]{5}'
+        result = re.search(lontl, doc[0])
+        if result:
+            return result.group(0)
+        return None
 
 
 if __name__ == '__main__':
     dirs = os.listdir()
     for file in dirs:
         if re.match('.+.pdf', file):
-            shutil.copy(file, f"{file.replace('.pdf','_')}{get_dlv(file)}.pdf")
+            extension = get_ext(file)
+            if extension:
+                shutil.copy(file, f"{file.replace('.pdf','_')}{extension}.pdf")
 
