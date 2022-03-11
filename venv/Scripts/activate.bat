a21f9c8ca3d2d4dd8918dca94908448567e96107
Rafał Kowalewski <rkowalewski@pl.indorama.net>
2022-03-11 15:39:01 +0100

    venv fix

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\rkowalewski\rpozpoznaj_nr_dokumentu\venv
+set VIRTUAL_ENV=C:\Users\rkowalewski\PycharmProjects\pdf_renamer\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
